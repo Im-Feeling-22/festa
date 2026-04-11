d65bfbd2f0ee8cfead121c820e00bb82c36ca443
Im-Feeling-22 <inkdeltasans@gmail.com>
2026-04-11 04:12:40 -0300

    Add files via upload

---
+++ catalogo.js
@@ -0,0 +1,57 @@
+let todasMusicas = [];
+
+// carregar o JSON
+fetch("../js/musicas.json")
+.then(res => res.json())
+.then(data => {
+
+    data.sort((a, b) => 
+    a.cantor.localeCompare(b.cantor, "pt-BR", { sensitivity: "base" })
+    );
+
+    todasMusicas = data;
+
+    mostrarMusicas(data);
+
+});
+
+// função que mostra as músicas na tabela
+function mostrarMusicas(lista){
+
+    const tbody = document.getElementById("catalogo");
+
+    tbody.innerHTML = "";
+
+    lista.forEach(m => {
+
+        const linha = document.createElement("tr");
+
+        linha.innerHTML = `
+        <td>${m.codigo}</td>
+        <td>${m.cantor}</td>
+        <td>${m.musica}</td>
+        `;
+
+        tbody.appendChild(linha);
+
+    });
+
+}
+
+// pesquisa
+document
+.getElementById("pesquisa")
+.addEventListener("input", function(){
+
+    const texto = this.value.toLowerCase();
+
+    const filtradas = todasMusicas.filter(m =>
+
+        m.musica.toLowerCase().includes(texto) ||
+        m.cantor.toLowerCase().includes(texto)
+
+    );
+
+    mostrarMusicas(filtradas);
+
+});
