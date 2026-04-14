a0f8f796658774957eca67cd909adc294567e977
Im-Feeling-22 <inkdeltasans@gmail.com>
2026-04-13 21:08:30 -0300

    update fotos.js (adicionei fotos manualmente ao site)

--- js/fotos.js
+++ js/fotos.js
@@ -11,9 +11,28 @@ window.addEventListener("load", () => {
 
     // Fotos já existentes
     const fotosExistentes = [
-      "https://res.cloudinary.com/dvpzenktl/image/upload/v1775635198/images_oiqbua.jpg",
-      "https://res.cloudinary.com/dvpzenktl/image/upload/v1775636478/pe6bspgpprcyyjyxxkvv.png",
-      "https://res.cloudinary.com/dvpzenktl/image/upload/v1775635052/convite_q2kk52.jpg",
+        // Resenha & Jogos
+      "https://res.cloudinary.com/dvpzenktl/image/upload/v1776123663/ccfbmx13gwyrl6aa6wqf.jpg",
+      "https://res.cloudinary.com/dvpzenktl/image/upload/v1776123285/mlyz4kmaqn494bjjcpb6.jpg",  
+      "https://res.cloudinary.com/dvpzenktl/video/upload/v1776123134/tjayuuceexyxtl5hotom.mp4",
+      "https://res.cloudinary.com/dvpzenktl/image/upload/v1776122917/t7qcpz2cy7txfcpysdis.jpg",
+      "https://res.cloudinary.com/dvpzenktl/image/upload/v1776122907/zhuizwstdl3qxokwnif3.jpg",
+      "https://res.cloudinary.com/dvpzenktl/image/upload/v1776122895/aqxk33vtnduihpw55a2z.jpg",
+        //Declarações
+      "https://res.cloudinary.com/dvpzenktl/video/upload/v1776123127/dpolluv2zrzslv2scpum.mp4",
+        //Karaoke
+      "https://res.cloudinary.com/dvpzenktl/video/upload/v1776123206/pqnspqmygfa7yt1k9y1j.mp4",
+      "https://res.cloudinary.com/dvpzenktl/video/upload/v1776123038/dce4oejnrht3fvtfrvjj.mp4",
+      "https://res.cloudinary.com/dvpzenktl/video/upload/v1776123003/ppvtekqrtvovi1lnc69n.mp4",
+        //Awards  
+      "https://res.cloudinary.com/dvpzenktl/image/upload/v1776123314/mapegh4ph9lcdebbirzx.jpg",
+        //Decoração
+      "https://res.cloudinary.com/dvpzenktl/image/upload/v1776123384/nuev7inzhp6tpypsnu8p.jpg",
+      "https://res.cloudinary.com/dvpzenktl/image/upload/v1776123372/t6e0wtbsd9tcttc9btya.jpg",
+        //Parabéns
+      "https://res.cloudinary.com/dvpzenktl/video/upload/v1776123280/w6pbnat5gy5p6jq3rvvk.mp4",
+        //Foto com Aniversariante
+      "https://res.cloudinary.com/dvpzenktl/image/upload/v1776123398/wgyeyhfqd4lnnnvieiba.jpg"  
     ];
 
     // Mostrar fotos existentes
